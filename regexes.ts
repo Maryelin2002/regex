b984146643f530d8ed93509b41bf5f905e34d927
Maryelin Virginia <maryelin.ramirez@solvex.com.do>
2022-11-23 17:02:26 -0400

    feat: initial

---
+++ regexes.ts
@@ -0,0 +1,11 @@
+/**
+ * Expresiones
+ */
+export const alphaNumeric: RegExp = RegExp(/([A-Za-z0-9])/)
+
+
+/**
+ * Expressions shortcuts
+ */
+export const alphaNumericShort: RegExp = RegExp(/(\w)/)
+export const numericShort: RegExp = RegExp(/(\d)/)
